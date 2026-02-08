c9846eda53b414c62de0abc568d83d86f4f5d1db
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-07 18:31:54 -0700

    Update lib.rs

--- crates/infranet-guard/src/lib.rs
+++ crates/infranet-guard/src/lib.rs
@@ -1,5 +1,6 @@
+
 mod guard;
 pub mod policy;
 
 pub use guard::InfranetGuard;
-pub use policy::{PolicyEngine, PolicyDecision};
+pub use policy::{NeurorightsPolicy, PolicyDecision, PolicyEngine, TsafeKernel};
